7e73db809f6485e2a141d4b7d1248d717628c710
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-04-20 12:49:04 +0200

    first commit

--- models/Post.js
+++ models/Post.js
@@ -25,6 +25,7 @@ const PostSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: false,
+    default: 'Anonymous',
   },
   email: {
     type: String,
