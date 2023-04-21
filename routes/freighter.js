126da76775282dc9f6a75be6e122b285b0343822
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-04-21 10:50:59 +0200

    first commit

--- routes/freighter.js
+++ routes/freighter.js
@@ -5,7 +5,10 @@ const User = require('../models/User');
 router.post("/", async (req, res, next) => {
 
   // console.log("newUser")
-
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Authorization,Content-Type,Accept,content-type,application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
     // validation 1
    const { pkey } = req.body;
