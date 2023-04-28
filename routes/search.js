3e820717f4f645dd1e5368a03711b45a0e474a0f
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-04-28 16:34:58 +0200

    first commit

--- routes/search.js
+++ routes/search.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const router = express.Router();
-const SearchData = require('../models/SearchData');
+const Cours = require('../models/Cours');
+const Post = require('../models/Post');
 
-router.get('/search', (req, res) => {
-  SearchData.find({}, (err, searchResults) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.json(searchResults);
-    }
-  });
+router.get('/', async (req, res) => {
+  try {
+    const searchQuery = req.query.searchQuery;
+    console.log('search' ,searchQuery);
+    const regex = new RegExp(searchQuery, "i");
+    
+    const courses = await Cours.find({
+      $or: [
+        { title: { $regex: regex } },
+        { description: { $regex: regex } },
+        { tags: { $regex: regex } }
+      ]
+    });
+    const posts = await Post.find({
+      $or: [
+        { title: { $regex: regex } },
+        { description: { $regex: regex } },
+        { tags: { $regex: regex } }
+      ]
+    });
+    
+    res.json({ courses, posts });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
 });
 
-module.exports = router;
 
+module.exports = router;
