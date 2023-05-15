08641e1119ce8346721cb81a32ae20f39fa6a9be
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-05-15 10:28:37 +0200

    first commit

--- routes/freighter.js
+++ routes/freighter.js
@@ -4,59 +4,50 @@ const User = require('../models/User');
 const crypto = require('crypto');
 router.post("/", async (req, res, next) => {
 
-  console.log("newUser")
+   console.log("newUser")
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  res.header('Content-Type', 'application/json');
-  // validation 1
-  const { pkey } = req.body;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // validation 1
+   const { pkey } = req.body;
 
-  // Generate a random email using crypto module
+   // Generate a random email using crypto module
   const pkeyEmail = pkey + '@edunode.org'
 
-  const newUser = {
-    pkey,
-    email: pkeyEmail
-  }
-
-  try {
-    let user = await User.findOne({ pkey: pkey })
-    if (user) {
-      jwt.sign(
-        { id: user.id }, process.env.JWT_SECRET,
-        { expiresIn: 3600 },
-        (err, token) => {
-          if (err) throw err;
-          res.send({
-        user: {
-          // granted: true,
-          pkey: user.pkey,
-          email: user.email,
-        }
-      });
-    });
-    } else {
-      user = await User.create(newUser)
-
-      res.send({
-        user: {
-          pkey: user.pkey,
-          email: user.email,
-
-        }
-      });
-
+    const newUser = {
+       pkey,
+       email: pkeyEmail
     }
+  
+    try {
+      let user = await User.findOne({ pkey: pkey })
+      if (user) {
+  
+        res.send({
+          user: {
+            // granted: true,
+            pkey: user.pkey,
+            email: user.email,
+          }
+        });
+      } else {
+        user = await User.create(newUser)
 
-  } catch (err) {
-    console.error(err)
-  }
-  next()
-});
+        res.send({
+          user: {
+            pkey: user.pkey,
+            email: user.email,
+
+          }
+        });
+  
+      }
+  
+    } catch (err) {
+      console.error(err)
+    }
+    next()
+  });
 
 
 
-module.exports = router;
+  module.exports = router;
