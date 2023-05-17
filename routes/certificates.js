f8337b1d3a761cc947e7806f3f6fef3c7517803f
olvisgil <olvisgil@gmail.com>
2023-05-17 15:10:54 +0200

    headers dimploma

--- routes/certificates.js
+++ routes/certificates.js
@@ -17,6 +17,11 @@ const bcrypt = require('bcryptjs');
 app.use(cors());
 
 router.post("/diploma", async (req, res) => {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  res.header('Content-Type', 'application/json');
 
   // Store the issuer and distributor key to mongoDB
 
