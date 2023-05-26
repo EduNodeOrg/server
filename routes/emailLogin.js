52298635d1630765e668e43bd3e3ea1cae42850c
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-05-26 12:28:35 +0200

    first commit

--- routes/emailLogin.js
+++ routes/emailLogin.js
@@ -8,6 +8,7 @@ const passport =require('passport');
 const cookieSession =require('cookie-session');
 router.post('/', function (req, res) {
    res.header("Access-Control-Allow-Origin", '*');
+   res.header("Access-Control-Allow-Origin", 'https://edunode.org');
    res.header("Access-Control-Allow-Credentials", true);
    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
