92b6585e05089395af6a69f2ce7d8b2c12892f8a
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-06-01 12:46:37 +0200

    trophy

--- routes/cours.js
+++ routes/cours.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth =require('../middleware/auth')
 const Cours = require("../models/Cours");
-
+const User = require('../models/User');
 // Create a new Cours
 router.post("/", async (req, res) => {
     try {
@@ -27,6 +27,32 @@ router.post("/", async (req, res) => {
     }
   });
   
+
+
+  router.put('/increment-trophy', async (req, res) => {
+    const { email } = req.body;
+  
+    try {
+      // Find the user by their email
+      const user = await User.findOne({ email });
+  
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      // Increment the trophy value by 1
+      user.AddCoursesTrophy += 1;
+  
+      // Save the updated user to the database
+      await user.save();
+  
+      return res.status(200).json({ message: 'Trophy incremented successfully' });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Server error' });
+    }
+  });
+
   
 // Get all Courss
 router.get("/cours", async (req, res) => {
