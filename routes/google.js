7e0f325fec4fb9891e0d6426c5a03e4a3388d748
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-06-06 17:12:28 +0200

    session

--- routes/google.js
+++ routes/google.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const User = require('../models/User');
 const jwt = require("jsonwebtoken");
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
+const domain = "edunode.org"
+const mg = mailgun.client({ username: 'api', key: "key-c8d12b7428fbe666e074108aaa0820bc" || 'key-yourkeyhere', url: 'https://api.eu.mailgun.net' });
+const session = require('express-session');
 
 router.post('/', function (req, res) {
   res.header("Access-Control-Allow-Origin", '*');
@@ -12,16 +18,46 @@ router.post('/', function (req, res) {
 
   const email = req.body.email;
   const name = req.body.name;
-  const images =req.body.image;
+  const images = req.body.image;
   console.log(email);
 
   User.findOne({ email })
     .then((user) => {
       if (user) {
+        // Store user data in the session
+        const sessionUser = {
+          name: name,
+          email: req.body.email,
+        };
+        req.session.user = sessionUser;
+
+        // Send email notification
+        const data = {
+          from: 'hi@edunode.org',
+          to: email,
+          subject: 'Welcome to Edunode ',
+          text: `Hello! Your have logged in to edunode!`
+        };
+        mg.messages.create(domain, data, function (error, body) {
+          if (error) {
+            console.log('Error sending email:', error);
+            res.status(500).json({ error: 'Error sending email' });
+          } else {
+            console.log('Email sent successfully:', body);
+            res.json({ msg: 'Email sent' });
+          }
+        });
+
+
         return res.status(200).json({ user, msg: "User already exists, welcome back" });
+
+
+
+
+
       } else {
         const confirmationCode = Math.floor(Math.random() * 90000) + 10000;
-        const newUser = new User({ email,name, confirmationCode,images });
+        const newUser = new User({ email, name, confirmationCode, images });
         newUser.save()
           .then(() => {
             // Generate JWT token and send it in response
@@ -36,9 +72,9 @@ router.post('/', function (req, res) {
                   user: {
                     id: newUser._id,
                     email: newUser.email,
-                    name:newUser.name,
+                    name: newUser.name,
                     confirmationCode: newUser.confirmationCode,
-                    images:newUser.images
+                    images: newUser.images
                   },
                 });
               }
@@ -56,5 +92,5 @@ router.post('/', function (req, res) {
     });
 });
 
-  
-    module.exports = router;
+
+module.exports = router;
