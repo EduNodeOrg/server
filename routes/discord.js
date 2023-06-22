d1533e3136004a4a297eaf4110ad0d3e2e20ef7d
olvisgil <olvisgil@gmail.com>
2023-06-22 14:39:09 +0200

    discord.js install

---
+++ routes/discord.js
@@ -0,0 +1,27 @@
+const {Client, IntentsBitField} = require("discord.js")
+
+const myIntents = new IntentsBitField();
+
+myIntents.add( IntentsBitField.Flags.Guilds,  IntentsBitField.Flags.GuildPresences, IntentsBitField.Flags.GuildMembers,         IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.DirectMessages);
+
+
+const client = new Client({
+    intents: myIntents
+})
+
+client.on("ready", (c) => {
+console.log(`${c.user.tag} is online`);
+})
+
+client.on("messageCreate", (message) => {
+console.log(message);
+if (message.mentions.users.has(client.user.id)) { // this could be modified to make it have to *start* with or have only a ping
+    message.reply("Hey!.")
+  } 
+});
+
+client.login(
+    "MTEyMDM1NDM3MTI4OTQ5NzcyMA.Go-iIp.idCT2BMmcEu8XS8SgWKIuLjIZEgkqkOmeq-0D8"
+);
