bfe737c882cf839f3ee1f61856a7384b61b23567
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-07-05 12:11:34 +0200

    admin dashboard

--- routes/valid.js
+++ routes/valid.js
@@ -13,35 +13,64 @@ router.get("/certificates", async (req, res) => {
   }
 });
 router.put('/edit-valid-certificates/:id', async (req, res) => {
-    const { id } = req.params;
-    const { status } = req.body;
-  
-    try {
-      const certificate = await ValidCertificate.findByIdAndUpdate(
-        id,
-        { status },
-        { new: true }
-      );
-      res.json(certificate);
-    } catch (error) {
-      console.error('Error updating certificate:', error);
-      res.status(500).json({ error: 'Failed to update certificate' });
-    }
-  });
-  
-  // Delete a valid certificate
-  router.delete('/delete-valid-certificates/:id', async (req, res) => {
-    const { id } = req.params;
-  
-    try {
-      await ValidCertificate.findByIdAndRemove(id);
-      res.json({ message: 'Certificate deleted successfully' });
-    } catch (error) {
-      console.error('Error deleting certificate:', error);
-      res.status(500).json({ error: 'Failed to delete certificate' });
-    }
-  });
-  
-  module.exports = router;
+  const { id } = req.params;
+  const { status } = req.body;
+
+  try {
+    const certificate = await ValidCertificate.findByIdAndUpdate(
+      id,
+      { status },
+      { new: true }
+    );
+    res.json(certificate);
+  } catch (error) {
+    console.error('Error updating certificate:', error);
+    res.status(500).json({ error: 'Failed to update certificate' });
+  }
+});
+
+// Delete a valid certificate
+router.delete('/delete-valid-certificates/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await ValidCertificate.findByIdAndRemove(id);
+    res.json({ message: 'Certificate deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting certificate:', error);
+    res.status(500).json({ error: 'Failed to delete certificate' });
+  }
+});
+
+// Route to return the number of certificates
+router.get("/count", async (req, res) => {
+  try {
+    const count = await ValidCertificate.countDocuments();
+    res.json({ count });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+// Route to return the number of certificates with an accepted status
+router.get("/acceptedCount", async (req, res) => {
+  try {
+    const count = await ValidCertificate.countDocuments({ status: "accepted" });
+    res.json({ count });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+// Route to return the number of certificates with a rejected status
+router.get("/rejectedCount", async (req, res) => {
+  try {
+    const count = await ValidCertificate.countDocuments({ status: "rejected" });
+    res.json({ count });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
 module.exports = router;
+
