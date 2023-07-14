f7c30057f2859efa5285f0be03c46a487ec5f479
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-07-14 13:08:00 +0200

    messages update

--- routes/messages.js
+++ routes/messages.js
@@ -12,16 +12,34 @@ router.post('/', messageController.createMessage);
 
 router.get('/count', async (req, res) => {
     try {
-      const { sender, receiver } = req.query;
+        const { sender, receiver } = req.query;
+    
+        let query = {
+          $or: [
+            { senderEmail: sender, receiverEmail: receiver },
+            { senderEmail: receiver, receiverEmail: sender },
+          ],
+        };
+    
+        // If the 'afterReset' query parameter is present and truthy, only consider messages sent after the reset
+        if (req.query.afterReset) {
+          query.createdAt = { $gt: new Date(req.query.afterReset) };
+        }
+    
+        const count = await Message.countDocuments(query);
+    
+        res.status(200).json({ count });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+      }
+  });
   
-      const count = await Message.countDocuments({
-        $or: [
-          { senderEmail: sender, receiverEmail: receiver },
-          { senderEmail: receiver, receiverEmail: sender },
-        ],
-      });
+  app.put('/reset', async (req, res) => {
+    try {
+      const { sender, receiver } = req.body;
   
-      // Update the message count for both sender and receiver
+      // Update the message count to 0 for the specified users
       await Message.updateMany(
         {
           $or: [
@@ -29,14 +47,15 @@ router.get('/count', async (req, res) => {
             { senderEmail: receiver, receiverEmail: sender },
           ],
         },
-        { $set: { messageCount: count } }
+        { $set: { messageCount: 0 } }
       );
   
-      res.status(200).json({ count });
+      res.status(200).json({ message: 'Message count reset successfully' });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Server error' });
     }
   });
-  
+
+
 module.exports = router;
