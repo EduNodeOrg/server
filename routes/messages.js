c8e56e07deadbd1f9512307822355b0ba5ae95d3
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-07-17 17:15:04 +0200

    message Notifications

--- routes/messages.js
+++ routes/messages.js
@@ -11,57 +11,78 @@ router.post('/', messageController.createMessage);
 
 
 router.get('/count', async (req, res) => {
-    try {
-      const { sender, receiver } = req.query;
-  
-      let query = {
+  try {
+    const { sender, receiver } = req.query;
+
+    let query = {
+      $or: [
+        { senderEmail: receiver, receiverEmail: sender },
+        { senderEmail: receiver, receiverEmail: sender },
+      ],
+    };
+
+    // If the 'afterReset' query parameter is present and truthy, only consider messages sent after the reset
+    if (req.query.afterReset) {
+      query.createdAt = { $gt: new Date(req.query.afterReset) };
+    }
+
+    const messages = await Message.find(query);
+
+    let count = 0;
+    messages.forEach((message) => {
+      count += message.messageCount;
+    });
+
+    res.status(200).json({ count });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+
+router.get('/countAll', async (req, res) => {
+  try {
+    const { sender } = req.body;
+
+    let query = {
+      receiverEmail: sender
+    };
+    const messages = await Message.find(query);
+
+    let count = 0;
+    messages.forEach((message) => {
+      count += message.messageCount;
+    });
+
+    res.status(200).json({ count });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+router.put('/reset', async (req, res) => {
+  try {
+    const { sender, receiver } = req.body;
+
+    // Update the message count to 0 for the specified users
+    await Message.updateMany(
+      {
         $or: [
           { senderEmail: receiver, receiverEmail: sender },
           { senderEmail: receiver, receiverEmail: sender },
         ],
-      };
-  
-      // If the 'afterReset' query parameter is present and truthy, only consider messages sent after the reset
-      if (req.query.afterReset) {
-        query.createdAt = { $gt: new Date(req.query.afterReset) };
-      }
-  
-      const messages = await Message.find(query);
-  
-      let count = 0;
-      messages.forEach((message) => {
-        count += message.messageCount;
-      });
-  
-      res.status(200).json({ count });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Server error' });
-    }
-  });
-  
-  
-  router.put('/reset', async (req, res) => {
-    try {
-      const { sender, receiver } = req.body;
-  
-      // Update the message count to 0 for the specified users
-      await Message.updateMany(
-        {
-          $or: [
-            { senderEmail: receiver, receiverEmail: sender },
-            { senderEmail: receiver, receiverEmail: sender },
-          ],
-        },
-        { $set: { messageCount: 0 } }
-      );
-  
-      res.status(200).json({ message: 'Message count reset successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Server error' });
-    }
-  });
+      },
+      { $set: { messageCount: 0 } }
+    );
+
+    res.status(200).json({ message: 'Message count reset successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
 
 
 module.exports = router;
