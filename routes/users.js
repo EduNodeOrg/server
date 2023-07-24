e32eb895dce993e464116a63b05767b2d1f4b506
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-07-24 15:15:35 +0200

    friend request email

--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,12 @@ const GoogleUser = require('../models/GoogleUser');
 const TwitterUser = require('../models/TwitterUser');
 dotenv.config({ path: './config/config.env' });
 const Notification = require("../models/Notification");
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
+const domain = "edunode.org"
+const mg = mailgun.client({ username: 'api', key: "key-c8d12b7428fbe666e074108aaa0820bc" || 'key-yourkeyhere', url: 'https://api.eu.mailgun.net' });
+
 
 
 // register email user
@@ -533,6 +539,30 @@ router.post('/friend-request/:userId', async (req, res) => {
 
     // Add the friend request
     receiver.friendRequests.push({ user: sender , userInfo: sender.name});
+
+    const data = {
+      from: 'hi@edunode.org',
+      to: user.email,
+      subject: 'Friend Request! ',
+      text: `Hello! Your have a new friend Request from ${sender.email} in Edunode!`
+    };
+
+
+    mg.messages.create(domain, data, function (error, body) {
+      if (error) {
+        console.log('Error sending email:', error);
+        res.status(500).json({ error: 'Error sending email' });
+      } else {
+        console.log('Email sent successfully:', body);
+        res.json({ msg: 'Email sent' });
+      }
+    });
+
+
+
+
+
+
     await receiver.save();
 
     res.status(200).json({ message: 'Friend request sent.' });
