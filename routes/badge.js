ad703348a531ebfdec89f86937eb6ba68131d5de
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-08-04 11:33:58 +0200

    updates

--- routes/badge.js
+++ routes/badge.js
@@ -15,6 +15,15 @@ router.post('/add', async (req, res) => {
   }
 });
 
+router.get('/posted/:email', async (req, res) => {
+  const {email} = req.params;
 
+  try {
+    const badges = await Badge.find({ email:email });
+    res.json(badges);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 module.exports = router;
